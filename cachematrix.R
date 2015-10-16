c61a7ab97274d6cfca0a9e2404a87dac60e41b59
pmtempone <pmtempone@gmail.com>
2015-10-16 00:36:37 -0300

    v1
    funciona get, pero getm devuelve NULL

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+          x <<- y
+          m <<- NULL
+        }
+        get <- function() x
+        prepare_matrix <- function(solve) m <<- solve
+        getM <- function() m
+        list(set = set, get = get,
+             prepare_matrix = prepare_matrix,
+             getM = getM)
 }
 
 
